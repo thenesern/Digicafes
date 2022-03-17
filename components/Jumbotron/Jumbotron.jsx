57f241ca3873c08e3092f52220f1500c35dac737
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-03-17 21:30:46 +0300

    Designed the Jumbotron

--- components/Jumbotron/Jumbotron.jsx
+++ components/Jumbotron/Jumbotron.jsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
 import React from "react";
 import styles from "./Jumbotron.module.css";
+import illustration from "../../assets/image/qr-menu.svg";
 
 const Jumbotron = () => {
-  return <div className={styles.container}>test</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <h1>Yeni Nesil Dijital Menü</h1>
+        <p className={styles.subHeader}>Baskı maliyetlerinden kurtulun.</p>
+        <button className={styles.button}>Hemen Başlayın</button>
+      </div>
+      <div className={styles.right}>
+        <Image
+          className={styles.image}
+          src={illustration}
+          alt="image"
+          width="600px"
+          height="600px"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Jumbotron;
