b523d08b507f770259a58ea53eb66a25284cd0e8
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-03-23 12:23:17 +0300

    Added "QRMenuDescription" Content

--- components/QRMenuDescription/QRMenuDescription.jsx
+++ components/QRMenuDescription/QRMenuDescription.jsx
@@ -1,14 +1,35 @@
 import React from "react";
-import QRMenuMockup from "../../assets/image/QRMenuMockup.png";
+import styles from "./QRMenuDescription.module.css";
 
 const QRMenuDescription = () => {
   return (
-    <div>
-      <div>
-        <img src={QRMenuMockup} alt="QR Menü Mockup" width="200" height="200" />
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img
+          src="https://raw.githubusercontent.com/thenesern/project/master/assets/image/QRMenuMockup.png"
+          alt="QR Menü Mockup"
+          className={styles.image}
+        />
       </div>
-      <div>
-        <h6>Baskı Maliyetlerinden Kurtulun</h6>
+      <div className={styles.right}>
+        <h1 className={styles.header}>Baskı Maliyetlerinden Kurtulun</h1>
+        <ul className={styles.list}>
+          <li>
+            <h5 className={styles.title}>Menünüz her zaman elinizin altında</h5>
+            <p>
+              QR Menü Yönetim Paneli ile menünüzü kolayca düzenleyebilirsiniz.
+            </p>
+          </li>
+          <li>
+            <h5 className={styles.title}>Kolay Erişim</h5>
+            <p>Sadece QR Kodunu okutarak, menüye ulaşın.</p>
+          </li>
+          <li>
+            <button className={styles.button}>
+              <span className={styles.btnText}>Hemen Başlayın</span>
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
