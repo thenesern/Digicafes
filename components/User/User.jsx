b05893091e6831c83390c28471af7abab2c96595
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-04-01 15:17:55 +0300

    Added Responsive Design to the User Profile Page

--- components/User/User.jsx
+++ components/User/User.jsx
@@ -46,53 +46,51 @@ const User = ({ order }) => {
   const [image, setImage] = useState("/defaultImage");
   const url = profile?.image;
   return (
-    <div className={styles.single}>
-      <div className={styles.top}>
-        <div className={styles.right}>
-          <h1 className={styles.title}>Kullanıcı Bilgileri</h1>
-          <form className={styles.details} onChange={formHandler}>
-            <img
-              src="https://img.icons8.com/external-flaticons-flat-flat-icons/452/external-user-web-flaticons-flat-flat-icons-2.png"
-              className={styles.img}
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <h1 className={styles.title}>Kullanıcı Bilgileri</h1>
+        <form className={styles.details} onChange={formHandler}>
+          <img
+            src="https://img.icons8.com/external-flaticons-flat-flat-icons/452/external-user-web-flaticons-flat-flat-icons-2.png"
+            className={styles.img}
+          />
+          <div className={styles.inputs}>
+            <TextField
+              className={styles.itemTitle}
+              helperText="Adınız"
+              value={firstName}
+              onChange={firstNameChangeHandler}
             />
-            <div className={styles.inputs}>
-              <TextField
-                className={styles.itemTitle}
-                helperText="Adınız"
-                value={firstName}
-                onChange={firstNameChangeHandler}
-              />
-              <TextField
-                className={styles.itemTitle}
-                helperText="Soyadınız"
-                value={lastName}
-                onChange={lastNameChangeHandler}
-              />
-              <TextField
-                className={styles.email}
-                helperText="Email"
-                value={profile?.email}
-                disabled
-              />
-            </div>
-            <button
-              disabled={!isChanged}
-              className={styles.button}
-              onClick={updateHandler}
-              type="submit"
-            >
-              Kaydet
-            </button>
-          </form>
-        </div>
-        <div className={styles.left}>
-          <h1 className={styles.title}>Siparişler</h1>
-          {order ? (
-            <List order={order} />
-          ) : (
-            <h6 className={styles.notFound}>Sipariş bulunamadı.</h6>
-          )}
-        </div>
+            <TextField
+              className={styles.itemTitle}
+              helperText="Soyadınız"
+              value={lastName}
+              onChange={lastNameChangeHandler}
+            />
+            <TextField
+              className={styles.email}
+              helperText="Email"
+              value={profile?.email}
+              disabled
+            />
+          </div>
+          <button
+            disabled={!isChanged}
+            className={styles.button}
+            onClick={updateHandler}
+            type="submit"
+          >
+            Kaydet
+          </button>
+        </form>
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>Siparişler</h1>
+        {order ? (
+          <List order={order} />
+        ) : (
+          <h6 className={styles.notFound}>Sipariş bulunamadı.</h6>
+        )}
       </div>
     </div>
   );
