b5b2721d5ba2355ffa47ee5527afeb2e9942f2e6
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-06-04 21:24:28 +0300

    Bugfix

--- pages/_app.js
+++ pages/_app.js
@@ -59,8 +59,9 @@ function MyApp({ Component, pageProps }) {
   gtag('config', '${process.env.GA_TRACKING_ID}');`}</Script>
       <Script
         id="google-tag-manager"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          _html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
