fe3c5c986513006d9afd0e1ac781b79296d6c202
Enes Eren <enes.eren@virtualitechnologies.com>
2022-07-03 20:12:35 +0300

    Store Showcase

---
+++ pages/booking/[storeLinkName]/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Booking from "../../../models/Booking";
+import Order from "../../../models/OrderModel.js";
+import db from "../../../utils/db";
+import Nav from "../../../components/Nav2/Nav";
+import Footer from "../../../components/Footer/Footer";
+import StoreBookingShowcase from "../../../components/StoreBookingShowcase";
+
+const StoreBookingProfile = ({ store }) => {
+  return (
+    <div>
+      <Nav />
+      <StoreBookingShowcase store={store} />
+      <Footer />
+    </div>
+  );
+};
+export async function getServerSideProps(context) {
+  const { storeLinkName } = context.query;
+  await db.connect();
+  const store = await Booking.findOne({
+    storeLinkName,
+  });
+
+  const order = await Order.findOne({ booking: store?._id });
+  const newDate = new Date();
+  if (
+    new Date(order?.expiry?.toString()).getTime() > newDate.getTime() ===
+    false
+  ) {
+    return {
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+    };
+  }
+
+  await db.disconnect();
+  return {
+    props: {
+      store: JSON.parse(JSON.stringify(store)),
+    },
+  };
+}
+export default StoreBookingProfile;
