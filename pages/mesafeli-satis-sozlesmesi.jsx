3b58df6caffcd765f1cec125683be4b3e75f4a14
Enes Eren <enes.eren@virtualitechnologies.com>
2022-07-07 18:57:05 +0300

    Bugfix

--- pages/mesafeli-satis-sozlesmesi.jsx
+++ pages/mesafeli-satis-sozlesmesi.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "components/Nav2/Nav";
+import Nav from "../components/Nav2/Nav";
 import Footer from "../components/Footer/Footer";
 import MesafeliSatisSozlesmesiPage from "../components/MesafeliSatisSozlesmesiPage/MesafeliSatisSozlesmesiPage";
 
