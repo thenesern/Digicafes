e67b28bc5d404473a93a653c854881dd9a7f779c
Enes Eren <enes.eren@virtualitechnologies.com>
2022-07-16 21:39:50 +0300

    Booking Dashboard - Prices

---
+++ pages/api/booking/[storeName]/prices.js
@@ -0,0 +1,23 @@
+import nc from "next-connect";
+import Booking from "../../../../models/Booking";
+import { isAuth } from "../../../../utils/auth";
+import db from "../../../../utils/db";
+
+const handler = nc();
+
+handler.use(isAuth);
+handler.post(async (req, res) => {
+  console.log(req.body);
+  await db.connect();
+  await Booking.findOneAndUpdate(
+    { storeName: req.body.storeName },
+    {
+      prices: req.body.prices,
+    }
+  );
+  const store = await Booking.findOne({ storeName: req.body.storeName });
+  res.json({ status: "success", store });
+  await db.disconnect();
+});
+
+export default handler;
