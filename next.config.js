5ae0c1680c3dba4d92e6874b4bbd66dd315a0611
Enes Eren <enes.eren@virtualitechnologies.com>
2022-07-17 13:58:47 +0300

    Bugfix

--- next.config.js
+++ next.config.js
@@ -12,8 +12,5 @@ module.exports = nextTranslate({
   },
   env: {
     GA_TRACKING_ID: "G-4Y5T6H6FBL",
-    TWILIO_SERVICE_SID: "MG2f914f2fc6bb38cf81209aeded06697a",
-    TWILIO_AUTH_TOKEN: "1a1cd30286bf145e487f337e4554af2d",
-    TWILIO_ACCOUNT_SID: "AC0e2c1253b45471a1db6890c831b78303",
   },
 });
